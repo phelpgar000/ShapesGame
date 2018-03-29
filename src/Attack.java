4efa32ae68b8787808ed05fc2add7e52da064cfa
phelpgar000 <phelpgao000@warren.k12.in.us>
2018-03-28 20:18:31 -0400

    commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit

---
+++ src/Attack.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+
+public class Attack extends Objects{
+
+    private boolean isRemove;
+
+    public Attack(Color color, int x, int y, int width, int height){
+        super(color, x, y, width, height);
+        isRemove = false;
+    }
+
+
+    public void move(int boardWidth, int boardHeight) {
+        if (x < 0 || x > boardWidth) {
+            isRemove = true;
+        }
+        if (y < 0 || y > boardHeight) {
+            isRemove = true;
+        }
+
+        y += -10;
+    }
+    @Override
+    public void paint(Graphics g){
+        g.setColor(color);
+        g.fillRect(x, y, width, height);
+    }
+
+    public boolean isRemove(){
+        return isRemove;
+    }
+}
